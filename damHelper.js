0d1b0df7e28324905e856090c311bce8a84f49cd
mureb <leonardomureb@gmail.com>
2021-04-21 19:22:28 -0500

    asd

--- damHelper.js
+++ damHelper.js
@@ -1,6 +1,6 @@
 
 
-//fetch('https://raw.githubusercontent.com/mureb/damTestScript/main/damHelper.js', {mode:'cors'}).then((response) => response.text()).then((text)=>eval(text))
+//fetch('https://raw.githubusercontent.com/mureb/damTestScript/master/damHelper.js', {mode:'cors'}).then((response) => response.text()).then((text)=>eval(text))
 
 
 var x = function(){
@@ -15,10 +15,55 @@ $("ot-resource").removeAttr('ot-onspecialselect')
 $("ot-resource").removeAttr('onclick')
 
 $("ot-resource").on( "click", function(event) {
-    console.log(this)
+    
+   var data = {
+        "bulk_edit_request_param":{
+           "bulk_edit_request":{
+              "context":{
+                 "asset_ids":[ $(this).attr('resourceid')
+                 ],
+                 "asssetContentType":[$(this).attr('resourcetype')
+                 ],
+                 "assetSubContentType":[
+                    "none"
+                 ],
+                 "type":"com.artesia.asset.selection.AssetIdsSelectionContext",
+                 "include_descendants":"NONE",
+                 "include_deleted_assets":false
+              },
+              "metadata_values":{
+                 "metadata_edit_id_value_list":[
+                    {
+                       "field_id_value_entries":[
+                          {
+                             "metadata_field_id":"KFT.FIELD.LACTALIS_STATUS",
+                             "metadata_value":{
+                                "value":{
+                                   "type":"string",
+                                   "value":"Approved To Move"
+                                }
+                             }
+                          }
+                       ],
+                       "metadata_section_id":"METADATA_MISC_EDIT_SECTION"
+                    }
+                 ]
+              }
+           }
+        }
+     };
+
+    console.log(data);
+
 });
 
 };
+
+
+//https://dam.kraftheinzcompany.com/otmmapi/v5/assets/
+/*
+method PATCH
+*/
 
 x();
 
