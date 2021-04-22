e6455659d7193aa729480db0b97ad42063363781
mureb <leonardomureb@gmail.com>
2021-04-22 12:57:40 -0500

    a

--- damHelper.js
+++ damHelper.js
@@ -16,9 +16,9 @@ $("ot-resource").removeAttr('onclick')
 
 $("ot-resource").on( "click", function(event) {
    
-   var assetId = $(this).attr('resourceid');
+   var assetID = $(this).attr('resourceid');
 
-   var editJson = {
+   var modifiedData = {
        "edited_asset":{
            "data":{
                "metadata":[{
@@ -34,68 +34,16 @@ $("ot-resource").on( "click", function(event) {
             }
         }
     }
-   
-   
-    /* 
-   var data = {
-        "bulk_edit_request_param":{
-           "bulk_edit_request":{
-              "context":{
-                 "asset_ids":[ $(this).attr('resourceid')
-                 ],
-                 "asssetContentType":[$(this).attr('resourcetype')
-                 ],
-                 "assetSubContentType":[
-                    "none"
-                 ],
-                 "type":"com.artesia.asset.selection.AssetIdsSelectionContext",
-                 "include_descendants":"NONE",
-                 "include_deleted_assets":false
-              },
-              "metadata_values":{
-                 "metadata_edit_id_value_list":[
-                    {
-                       "field_id_value_entries":[
-                          {
-                             "metadata_field_id":"KFT.FIELD.LACTALIS_STATUS",
-                             "metadata_value":{
-                                "value":{
-                                   "type":"string",
-                                   "value":"Approved To Move"
-                                }
-                             }
-                          }
-                       ],
-                       "metadata_section_id":"METADATA_MISC_EDIT_SECTION"
-                    }
-                 ]
-              }
-           }
-        }
-     };*/
 
-     $.ajax({
-        headers : {
-            'Accept' : '*/*',
-            'Content-Type' : 'application/json, charset=UTF-8',
-            'x-otmm-locale': 'en_US',
-            'x-requested-by': 1536986997
-        },
-        url : '/otmmapi/v5/assets/' + assetId,
-        type : 'PATCH',
-        data : JSON.stringify(editJson),
-        success : function(response, textStatus, jqXhr) {
-            console.log("Success!!");
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-            // log the error to the console
-            console.log("The following error occured: " + textStatus, errorThrown);
-        },
-        complete : function() {
-            console.log("Code ran!!");
-        }
+    var data = {'assetID':assetID, 'modifiedData':modifiedData}
+   
+
+    AssetDetailManager.update (data, function(response, status, success){
+
+        console.log('it works');
+        console.lof(response);
+            
     });
-
 });
 
 };
