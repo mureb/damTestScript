f6285c7d8639da21ed5a08ae8285f487acc249be
mureb <leonardomureb@gmail.com>
2021-04-22 15:01:25 -0500

    a

--- json.js
+++ json.js
@@ -154,3 +154,119 @@ x-requested-by: 1536986997
 x-requested-with: XMLHttpRequest
 
 
+
+
+
+:authority: dam.kraftheinzcompany.com
+:method: PATCH
+:path: /otmmapi/v5/assets/2b579e614ae37ab9c0198486d65a98eea0284295
+:scheme: https
+accept: */*
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9,pt;q=0.8
+content-length: 182
+content-type: application/json, charset=UTF-8
+cookie: JSESSIONID=OlLgyuP0n09FDklmgAuWg6F-Y9njhbVEjBgNP1ho.OTMMPROD1; _ga=GA1.2.157103454.1617807653; AWSALB=ZkSxs1UwdZHTBUPGvqAdnE6XXPBiqgJT/2PJyMY4s8Qflxlxqjja+lej4KEc0qabzdFe3lY6xuBSd/oKToQeDd2dfBB1iBWgkXv+oZV7+szGDaKa2B9O+wkDUjz5; AWSALBCORS=ZkSxs1UwdZHTBUPGvqAdnE6XXPBiqgJT/2PJyMY4s8Qflxlxqjja+lej4KEc0qabzdFe3lY6xuBSd/oKToQeDd2dfBB1iBWgkXv+oZV7+szGDaKa2B9O+wkDUjz5
+origin: https://dam.kraftheinzcompany.com
+referer: https://dam.kraftheinzcompany.com/
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: same-origin
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
+x-otmm-locale: en_US
+x-requested-by: 1536986997
+x-requested-with: XMLHttpRequest
+
+
+
+:authority: dam.kraftheinzcompany.com
+:method: PATCH
+:path: /otmmapi/v5/assets/2b579e614ae37ab9c0198486d65a98eea0284295
+:scheme: https
+accept: */*
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9,pt;q=0.8
+content-length: 182
+content-type: application/json; charset=UTF-8
+cookie: JSESSIONID=OlLgyuP0n09FDklmgAuWg6F-Y9njhbVEjBgNP1ho.OTMMPROD1; _ga=GA1.2.157103454.1617807653; AWSALB=eUSaM9BzUWJ9Z0UJdShkwWqDOF+YrVNRmyuoPH+Cse+5itDNtTGivyBHJOmLB08E4smpJGuR1DmbAc12Ng3xRauYT9fN1nZlUyUwPfWoYUcMeHbnm/UFN6IdXa1C; AWSALBCORS=eUSaM9BzUWJ9Z0UJdShkwWqDOF+YrVNRmyuoPH+Cse+5itDNtTGivyBHJOmLB08E4smpJGuR1DmbAc12Ng3xRauYT9fN1nZlUyUwPfWoYUcMeHbnm/UFN6IdXa1C
+origin: https://dam.kraftheinzcompany.com
+referer: https://dam.kraftheinzcompany.com/
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: same-origin
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
+x-otmm-locale: en_US
+x-requested-by: 12988112
+x-requested-with: XMLHttpRequest
+
+
+:authority: dam.kraftheinzcompany.com
+:method: PATCH
+:path: /otmmapi/v5/assets/4a944034c8ff65cae316ead5e53276fe5c2af7e3
+:scheme: https
+accept: */*
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9,pt;q=0.8
+content-length: 182
+content-type: application/json; charset=UTF-8
+cookie: JSESSIONID=OlLgyuP0n09FDklmgAuWg6F-Y9njhbVEjBgNP1ho.OTMMPROD1; _ga=GA1.2.157103454.1617807653; AWSALB=whZZwowXPFUmNC4e/cu/3pKyDCbHICW8eKno+fsMJ1cBA/9FsUw6nEzjZJ1V1dqPHGOyo7nF1//emqfsvuSs5kJMsqyEAafsMxK6D6kNPIw4LxInCjCE7JUmdyvd; AWSALBCORS=whZZwowXPFUmNC4e/cu/3pKyDCbHICW8eKno+fsMJ1cBA/9FsUw6nEzjZJ1V1dqPHGOyo7nF1//emqfsvuSs5kJMsqyEAafsMxK6D6kNPIw4LxInCjCE7JUmdyvd
+origin: https://dam.kraftheinzcompany.com
+referer: https://dam.kraftheinzcompany.com/
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: same-origin
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
+x-otmm-locale: en_US
+x-requested-by: 12988112
+x-requested-with: XMLHttpRequest
+
+
+
+
+
+AssetDetailManager.update = function update(data, callback)
+{
+    var assetID = data.assetID;
+    var json = data.modifiedData;
+    var serviceUrl = '/assets/' + assetID ;
+    otui.patch(otui.service + serviceUrl, JSON.stringify(json), otui.contentTypes.json,  function(response, status, success)
+    {
+        callback(response, status, success);
+    });
+};
+
+
+
+$.ajax({
+    headers : {
+        'Accept' : '*/*',
+        'Content-Type' : 'application/json, charset=UTF-8',
+        'x-otmm-locale': 'en_US',
+        'x-requested-by': 1536986997
+    },
+    url : '/otmmapi/v5/assets/' + assetId,
+    type : 'PATCH',
+    data : JSON.stringify(editJson),
+    success : function(response, textStatus, jqXhr) {
+        console.log("Success!!");
+    },
+    error : function(jqXHR, textStatus, errorThrown) {
+        // log the error to the console
+        console.log("The following error occured: " + textStatus, errorThrown);
+    },
+    complete : function() {
+        console.log("Code ran!!");
+    }
+});
+
+
+
+
+
+/**
+		 * Lock the asset using asset Id. Uses REST API PUT: /v1/assets/{id}/state?action=lock to lock the asset.
+		 *
+		 * @param data requires data.assetID = Asset Id
+		 * @param callback the function handler to execute, once the asset is locked.
+		 */
+ 
